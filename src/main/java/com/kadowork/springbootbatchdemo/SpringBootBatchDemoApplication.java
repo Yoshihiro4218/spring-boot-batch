5b09f3d60e705f4c3d10d2c54ab6bff18a1a2be3
yoshihiro <y.kadono4218@gmail.com>
2022-04-17 15:23:50 +0900

    時間計測できるように

--- src/main/java/com/kadowork/springbootbatchdemo/SpringBootBatchDemoApplication.java
+++ src/main/java/com/kadowork/springbootbatchdemo/SpringBootBatchDemoApplication.java
@@ -9,6 +9,11 @@ import org.springframework.boot.autoconfigure.*;
 public class SpringBootBatchDemoApplication {
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         SpringApplication.run(SpringBootBatchDemoApplication.class, args);
+        long endTime = System.currentTimeMillis();
+        System.out.println("開始時刻：" + startTime + " ms");
+        System.out.println("終了時刻：" + endTime + " ms");
+        System.out.println("処理時間：" + (endTime - startTime) + " ms");
     }
 }
