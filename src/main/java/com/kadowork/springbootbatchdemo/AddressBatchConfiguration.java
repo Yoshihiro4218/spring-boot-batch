7edd7ba3310d1399ec5696a166d110ea0a2b0801
yoshihiro <y.kadono4218@gmail.com>
2022-06-09 23:19:35 +0900

    for aws fargate

--- src/main/java/com/kadowork/springbootbatchdemo/AddressBatchConfiguration.java
+++ src/main/java/com/kadowork/springbootbatchdemo/AddressBatchConfiguration.java
@@ -1,99 +1,99 @@
-//package com.kadowork.springbootbatchdemo;
-//
-//import lombok.*;
-//import lombok.extern.slf4j.*;
-//import org.springframework.batch.core.*;
-//import org.springframework.batch.core.configuration.annotation.*;
-//import org.springframework.batch.core.launch.support.*;
-//import org.springframework.batch.item.file.*;
-//import org.springframework.batch.item.file.mapping.*;
-//import org.springframework.batch.item.file.transform.*;
-//import org.springframework.context.annotation.*;
-//import org.springframework.core.env.*;
-//import org.springframework.core.io.*;
-//
-//@Configuration
-//@EnableBatchProcessing
-//@AllArgsConstructor
-//@Slf4j
-//public class AddressBatchConfiguration {
-//    private final JobBuilderFactory jobBuilderFactory;
-//    private final StepBuilderFactory stepBuilderFactory;
-//    private final AddressTaskletHelper addressTaskletHelper;
-//    private final Environment environment;
-//
-//    private static final String CSV_FILE_PATH = "zenkoku.csv";
-//
-//    // chunk
+package com.kadowork.springbootbatchdemo;
+
+import lombok.*;
+import lombok.extern.slf4j.*;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.*;
+import org.springframework.batch.core.launch.support.*;
+import org.springframework.batch.item.file.*;
+import org.springframework.batch.item.file.mapping.*;
+import org.springframework.batch.item.file.transform.*;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.*;
+import org.springframework.core.io.*;
+
+@Configuration
+@EnableBatchProcessing
+@AllArgsConstructor
+@Slf4j
+public class AddressBatchConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final AddressTaskletHelper addressTaskletHelper;
+    private final Environment environment;
+
+    private static final String CSV_FILE_PATH = "zenkoku.csv";
+
+    // chunk
+    @Bean
+    public Job addressChunkJob() {
+        return jobBuilderFactory
+                .get("addressChunkJob")
+                .incrementer(new RunIdIncrementer())
+                .start(addressChunkStep())
+                .build();
+    }
+
+    @Bean
+    public Step addressChunkStep() {
+        int chunkSize = Integer.parseInt(environment.getProperty("chunk", "1000"));
+        log.info("[ChunkSize] {}", chunkSize);
+        return stepBuilderFactory.get("step1")
+                                 .<Address, Address>chunk(chunkSize)
+                                 .reader(addressChunkReader())
+                                 .processor(addressChunkProcessor())
+                                 .writer(addressChunkWriter())
+                                 .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<Address> addressChunkReader() {
+        log.info("====== Read Address ======");
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setNames("id", "prefCd", "cityCd", "townCd", "zip", "officeFlg", "deleteFlg",
+                               "prefName", "prefKana", "cityName", "cityKana", "townName", "townKana",
+                               "townMemo", "kyotoStreet", "azaName", "azaKana", "memo", "officeName",
+                               "officeKana", "officeAddress", "newId");
+
+        BeanWrapperFieldSetMapper<Address> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Address.class);
+
+        DefaultLineMapper<Address> lineMapper = new DefaultLineMapper<>();
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+
+        FlatFileItemReader<Address> reader = new FlatFileItemReader<>();
+        reader.setResource(new ClassPathResource(CSV_FILE_PATH));
+        reader.setLinesToSkip(1);
+        reader.setEncoding("Shift_JIS");
+        reader.setLineMapper(lineMapper);
+        return reader;
+    }
+
+    @Bean
+    public AddressItemProcessor addressChunkProcessor() {
+        return new AddressItemProcessor();
+    }
+
+    @Bean
+    public AddressItemWriter addressChunkWriter() {
+        return new AddressItemWriter();
+    }
+
+    // tasklet
 //    @Bean
-//    public Job addressChunkJob() {
+//    public Job addressTaskletJob() {
 //        return jobBuilderFactory
-//                .get("addressChunkJob")
+//                .get("addressTaskletJob")
 //                .incrementer(new RunIdIncrementer())
-//                .start(addressChunkStep())
+//                .start(addressTaskletStep())
 //                .build();
 //    }
 //
 //    @Bean
-//    public Step addressChunkStep() {
-//        int chunkSize = Integer.parseInt(environment.getProperty("chunk", "1000"));
-//        log.info("[ChunkSize] {}", chunkSize);
-//        return stepBuilderFactory.get("step1")
-//                                 .<Address, Address>chunk(chunkSize)
-//                                 .reader(addressChunkReader())
-//                                 .processor(addressChunkProcessor())
-//                                 .writer(addressChunkWriter())
+//    public Step addressTaskletStep() {
+//        return stepBuilderFactory.get("addressTaskletStep")
+//                                 .tasklet(addressTaskletHelper)
 //                                 .build();
 //    }
-//
-//    @Bean
-//    public FlatFileItemReader<Address> addressChunkReader() {
-//        log.info("====== Read Address ======");
-//        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
-//        lineTokenizer.setNames("id", "prefCd", "cityCd", "townCd", "zip", "officeFlg", "deleteFlg",
-//                               "prefName", "prefKana", "cityName", "cityKana", "townName", "townKana",
-//                               "townMemo", "kyotoStreet", "azaName", "azaKana", "memo", "officeName",
-//                               "officeKana", "officeAddress", "newId");
-//
-//        BeanWrapperFieldSetMapper<Address> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
-//        fieldSetMapper.setTargetType(Address.class);
-//
-//        DefaultLineMapper<Address> lineMapper = new DefaultLineMapper<>();
-//        lineMapper.setLineTokenizer(lineTokenizer);
-//        lineMapper.setFieldSetMapper(fieldSetMapper);
-//
-//        FlatFileItemReader<Address> reader = new FlatFileItemReader<>();
-//        reader.setResource(new ClassPathResource(CSV_FILE_PATH));
-//        reader.setLinesToSkip(1);
-//        reader.setEncoding("Shift_JIS");
-//        reader.setLineMapper(lineMapper);
-//        return reader;
-//    }
-//
-//    @Bean
-//    public AddressItemProcessor addressChunkProcessor() {
-//        return new AddressItemProcessor();
-//    }
-//
-//    @Bean
-//    public AddressItemWriter addressChunkWriter() {
-//        return new AddressItemWriter();
-//    }
-//
-//    // tasklet
-////    @Bean
-////    public Job addressTaskletJob() {
-////        return jobBuilderFactory
-////                .get("addressTaskletJob")
-////                .incrementer(new RunIdIncrementer())
-////                .start(addressTaskletStep())
-////                .build();
-////    }
-////
-////    @Bean
-////    public Step addressTaskletStep() {
-////        return stepBuilderFactory.get("addressTaskletStep")
-////                                 .tasklet(addressTaskletHelper)
-////                                 .build();
-////    }
-//}
+}
